a8d20c8c74acd50a003671b0704b3e48d0ad0d61
kaikai <gamera1000x@gmail.com>
2024-02-28 01:50:02 +0900

    readText()が実行されない。なあぜなあぜ

---
+++ AsyncAwaitPractice/LinesTestView.swift
@@ -0,0 +1,18 @@
+//
+//  LinesTestView.swift
+//  AsyncAwaitPractice
+//
+//  Created by 青山凱 on 2024/02/28.
+//
+
+import SwiftUI
+
+struct LinesTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinesTestView()
+}
---
+++ AsyncAwaitPractice/LinesTestViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LinesTestViewModel.swift
+//  AsyncAwaitPractice
+//
+//  Created by 青山凱 on 2024/02/28.
+//
+
+import Foundation
