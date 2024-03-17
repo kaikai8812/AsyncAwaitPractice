f620d5ebbc041f7b2cb33c07bc8d37df753893cc
kaikai <gamera1000x@gmail.com>
2024-03-17 18:29:06 +0900

    group.nextを使用して、並列、非同期で取得できた値を順次処理する。 非同期、一回目で取得できた情報によって、後続の処理を変化させる。

--- AsyncAwaitPractice/4-TasK/withTaskGroupView.swift
+++ AsyncAwaitPractice/4-TasK/withTaskGroupView.swift
@@ -29,14 +29,14 @@ struct withTaskGroupView: View {
     
     private func getUserAge() async -> Int {
         await Util.wait(second: 1)
-        return 21
+        return 20
     }
     
     func fetchUserData() async -> UserInfo {
         var name: String?
         var age: Int?
         
-        enum FetchType {
+        enum FetchType: Equatable {
             case name(String)
             case age(Int)
         }
@@ -57,14 +57,55 @@ struct withTaskGroupView: View {
                 return FetchType.age(age)
             }
             
-            for await fetchResult in group {
-                switch fetchResult {
-                case .name(let n):
-                    name = n
-                case .age(let a):
-                    age = a
-                }
+            /// for await in ループで結果を取得する場合
+//            for await fetchResult in group {
+//                switch fetchResult {
+//                case .name(let n):
+//                    name = n
+//                case .age(let a):
+//                    age = a
+//                }
+//            }
+            
+            /// nextメソッドで、順次情報を取得する場合
+            guard let first = await group.next() else {
+                group.cancelAll()
+                    return
             }
+            
+            switch first {
+            case .name(let n):
+                print("一回目:\(n)")
+                name = n
+            case .age(let a):
+                print("一回目:\(a)")
+                age = a
+            }
+            
+            /// nextを使用することで
+            /// 一つ目に取得できた値によって、後続の処理を変更することができる。
+            /// 21歳だということがわかったら、nameはAPIで取得することをやめて、文字列を追加する。
+            if first == .age(21) {
+                name = "21歳の誰か"
+                group.cancelAll()
+                return
+            }
+            
+            
+            guard let second = await group.next() else {
+                group.cancelAll()
+                return
+            }
+            
+            switch second {
+            case .name(let n):
+                print("2回目:\(n)")
+                name = n
+            case .age(let a):
+                print("2回目:\(a)")
+                age = a
+            }
+            
         }
         return UserInfo(name: name ?? "名前取得失敗", age: age ?? 100)
     }
