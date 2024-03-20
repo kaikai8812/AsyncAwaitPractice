a2b3bf2511a786091ab51ca876cd19d5e0f4609a
kaikai <gamera1000x@gmail.com>
2024-03-20 22:27:30 +0900

    withThrowingTaskGroupを使用して、子タスクでエラーを投げる (4.5 エラーが発生するタスクグループ)

--- AsyncAwaitPractice/4-TasK/ConcurrencyTaskDoView.swift
+++ AsyncAwaitPractice/4-TasK/ConcurrencyTaskDoView.swift
@@ -14,17 +14,22 @@ struct 動的に非同期なタスクを実行するView: View {
     var body: some View {
         Button("取得") {
             Task {
-                var s = await fetchValues(ids: ["1", "2", "3", "4"])
-                print("取得できた結果: \(s)")
+                do {
+                    var data = try await fetchValues(ids: ["1", "2", "3", "4"])
+                    print("取得できた結果: \(data)")
+                } catch {
+                    print("呼び出し元で、error発生を検知")
+                    print(error.localizedDescription)
+                }
+                
             }
         }
     }
     
-    private func fetchValues(ids: [String]) async -> [String: String?] {
+    private func fetchValues(ids: [String]) async throws -> [String: String?] {
         
         // 非同期で、配列分の子タスクを作成する
-        
-        return await withTaskGroup(of: (String, String?).self) { group in
+        return try await withThrowingTaskGroup(of: (String, String?).self) { group in
             for id in ids {
                 group.addTask {
                     print("子タスク追加が完了したもの:\(id)")
@@ -32,10 +37,17 @@ struct 動的に非同期なタスクを実行するView: View {
                 }
             }
             
+            /// ここで追加した子タスクが、エラーを発生させる。
+            /// 同じ子タスクとして追加された、getValueを行う子タスクも、ここで追加した子タスクが
+            /// エラーになることで、エラーが伝番し、タスクが終了されることが確認できる。
+            group.addTask {
+                return ("error", try await getValueWithError(isError: true))
+            }
+            
             var values: [String: String?] = [:]
             
             // 子タスクは、並列で実行される。
-            for await (id, value) in group {
+            for try await (id, value) in group {
                 print("データ取得：id: \(id), value: \(String(describing: value))")
                 values[id] = value
             }
@@ -44,13 +56,22 @@ struct 動的に非同期なタスクを実行するView: View {
             return values
             
         }
-        
     }
     
     private func getvalue(id: String) async -> String {
-        await Util.wait(second: 1)
+        await Util.wait(second: 3)
         return "\(id + "valueです！")"
     }
+    
+    private func getValueWithError(isError: Bool) async throws -> String {
+        await Util.wait(second: 1)
+        if isError { throw SampleError.TestError("テストエラーが発生しました。") }
+        return "成功"
+    }
+}
+
+enum SampleError: Error {
+    case TestError(String)
 }
 
 #Preview {
